d5c872299f97bb8b275c7ca934dd3f7d35d4d4f6
pmk2424 <pmk4236@naver.com>
2021-04-28 11:05:06 +0900

    Header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 import logo from "../media/image/triportlogo.png";
-import "./Header.css";
 
 const Header = () => {
   return (
