25275d3e60a2b190abe1e0d0e9d7ba9891a527f9
kbyunghoon <juice1231102@naver.com>
2021-04-28 17:21:55 +0900

    이미지,svg 파일 정리 및 슬라이드 초안

---
+++ src/TestData.js
@@ -0,0 +1,14 @@
+export const TestData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1619544151588-7147a2a55558?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1619440482145-3133e2abb77d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1619478692045-181a130fcb0f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1348&q=80",
+  },
+];
