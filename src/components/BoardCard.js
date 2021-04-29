d07445372abd25b7482d93a4bf99a3d08c40f771
Ray Jeong <rayrayj92@gmail.com>
2021-04-29 20:10:46 +0900

    게시판 페이지

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -1,10 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { LikeFill, LikeEmpty } from "media/svg/Svg";
 
 const BoardCard = (props) => {
-    return(
-        <BoardCardContainer>
+    const { margin } = props;
 
+    return(
+        <BoardCardContainer margin={ margin } >
+            <ImageContainer>
+                <UserContainer>
+                    <Profile>
+                        <div></div>
+                        <span>Triport</span>
+                    </Profile>
+                    <Likes>
+                        <LikeFill />
+                    </Likes>
+                </UserContainer>
+            </ImageContainer>
+            <ContentContainer>
+                <Detail>
+                    <span>2021년 04월 29일</span>
+                    <span>13:00:45</span>
+                </Detail>
+                <Title>
+                    <span>에디터가 추천하는 관악 여행</span>
+                </Title>
+                <Description>
+                    <span>맛집이 가득한 샤로수길을 소개합니다~</span>
+                </Description>
+            </ContentContainer>
         </BoardCardContainer>
     );
 };
@@ -12,9 +37,99 @@ const BoardCard = (props) => {
 const BoardCardContainer = styled.div`
     display: inline-block;
     box-sizing: border-box;
-    width: 400px;
-    height: 400px;
-    border: 1px solid #efefef;
+    width: 14rem;
+    height: 23.063rem;
+    ${(props) => (props.margin ? `margin: ${props.margin};` : '')};
 `; 
+
+const ImageContainer = styled.div`
+    position: relative;
+    width: 100%;
+    height: 14rem;
+    background: url('http://www.rvsfinance.nl/wp-content/uploads/2016/06/ef3-placeholder-image.jpg') no-repeat center;
+    background-size: cover;
+    cursor: pointer;
+`;
+
+const UserContainer = styled.div`
+    position: absolute;
+    bottom: 11.5px;
+    width: 100%;
+    height: 1.563rem;
+    padding: 0 15px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    & div {
+        width: 1.563rem;
+        height: 1.563rem;
+        background: url('https://microbiology.ucr.edu/sites/g/files/rcwecm2866/files/styles/form_preview/public/blank-profile-pic.png?itok=xMM7pLfb') no-repeat center;
+        background-size: cover;
+        border-radius: 50%;
+    }
+    & span {
+        margin-left: 5px;
+        font-size: 12px;
+        color: #5A5A5A;
+        font-weight: 600;
+    }
+`;
+
+const Likes = styled.div`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    z-index: 9999;
+    & svg {
+        width: 1.643rem;
+    }
+`;
+
+const ContentContainer = styled.div`
+    width: 100%;
+    height: 9.063rem;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #BCBCBC;
+    border-radius: 5px;
+    margin-top: 2px;
+`;
+
+const Detail = styled.div`
+    box-sizing: content-box;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    & span {
+        font-size: 12px;
+        color: #5A5A5A;
+    }
+`;
+
+const Title = styled.div`
+    box-sizing: content-box;
+    text-align: center;
+    cursor: pointer;
+    & span {
+        color : #2B61E1;
+    }
+`;
+
+const Description = styled.div`
+    box-sizing: content-box;
+    padding: 10px 20px;
+    text-align: center;
+    & span {
+        font-size: 12px;
+        color: #5A5A5A;
+    }
+`;
 
 export default BoardCard;
--- src/pages/BoardMain.js
+++ src/pages/BoardMain.js
@@ -1,13 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import "shared/scss/BoardMain.scss";
 import { BoardCard } from 'components/components';
 
 const BoardMain = (props) => {
+
+    const tabToggle = (e) => {
+        const tab = document.getElementById('BoardTab');
+        const editor = document.getElementById('TravelEditorText');
+        const traveler = document.getElementById('TravelerText');
+
+        if(tab.style.left !== '50%') {
+            tab.style.left = '50%';
+            editor.style.color = '#89ACFF';
+            traveler.style.color = '#fff';
+        } else {
+            tab.style.left = '0%';
+            editor.style.color = '#fff';
+            traveler.style.color = '#89ACFF';
+        }
+    };
+
     return (
         <BoardMainContainer>
-            <BoardCard />
-            <BoardCard />
-            <BoardCard />
+            <FilterContainer>
+                <Editor>
+                    <Background id="BoardTab" />
+                    <TravelEditor onClick={tabToggle} className="TravelEditor">
+                        <span className="TravelEditor" id="TravelEditorText">트레블 에디터</span>
+                    </TravelEditor>
+                    <Traveler onClick={tabToggle} className="Traveler">
+                        <span className="Traveler" id="TravelerText">트레블러</span>
+                    </Traveler>
+                </Editor>
+                <Filter className="BoardFilterContainer">
+                    <FilterSelect id="BoardFilter" className="BoardFilterSelect">
+                        <option value="likes" selected>좋아요순</option>
+                        <option value="newest" >최신순</option>
+                    </FilterSelect>
+                </Filter>
+            </FilterContainer>
+            <CardContainer>
+                <BoardCard margin='0 40px 0 0' />
+                <BoardCard margin='0 40px 0 0' />
+                <BoardCard margin='0 40px 0 0' />
+                <BoardCard margin='0 40px 0 0' />
+                <BoardCard />
+
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard />
+
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard />
+
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard />
+
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard margin='50px 40px 0 0' />
+                <BoardCard />
+            </CardContainer>
         </BoardMainContainer>
     );
 };
@@ -15,6 +78,92 @@ const BoardMain = (props) => {
 const BoardMainContainer = styled.div`
     width: 1280px;
     margin: 0 auto;
+`;
+
+const FilterContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 50px;
+`;
+
+const Editor = styled.div`
+    position: relative;
+    width: 14.125rem;
+    height: 2.5rem;
+    background: #F4F4F4 0% 0% no-repeat padding-box;
+    box-shadow: 0px 3px 6px #00000029;
+    border-radius: 5px;
+    display: flex;
+`;
+
+const Background = styled.div`
+    position: absolute;
+    width: 50%;
+    height: 100%;
+    left: 0;
+    background: #2B61E1 0% 0% no-repeat padding-box;
+    border-radius: 5px;
+    transition: left .5s;
+`;
+
+const TravelEditor = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    & span {
+        font-family: 'TTTogether';
+        font-size: 14px;
+        letter-spacing: 0px;
+        color: #fff;
+        z-index: 9999;
+        transition: color .3s;
+    }
+`;
+
+const Traveler = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    & span {
+        font-family: 'TTTogether';
+        font-size: 14px;
+        letter-spacing: 0px;
+        color: #89ACFF;
+        z-index: 9999;
+        transition: color .3s;
+    }
+`;
+
+const Filter = styled.div`
+    position: relative;
+    width: 5rem;
+    display: flex;
+    align-items: center;
+`;
+
+const FilterSelect = styled.select`
+    width: 5rem;
+    height: 2.5rem;
+    border-radius: 5px;
+    cursor: pointer;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    font-family: 'AppleSDGothicNeoR', sans-serif;
+    font-size: 14px;
+    color: #5A5A5A;
+`;
+
+const CardContainer = styled.div`
+    width: 100%;
 `;
 
 export default BoardMain;
