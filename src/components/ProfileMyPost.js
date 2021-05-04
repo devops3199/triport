7abde2692202ed684e9a469a1e92a9c550eed1cc
pmk2424 <pmk4236@naver.com>
2021-05-04 17:38:19 +0900

    내가 쓴 글 component 구현 완료

--- src/components/ProfileMyPost.js
+++ src/components/ProfileMyPost.js
@@ -2,40 +2,72 @@ import React from "react";
 import styled from "styled-components";
 
 import Dmypost from "media/svg/내가 쓴 글 D.svg";
-import BoardCard from "components/components";
+import { BoardCard } from "components/components";
 import SnsPost from "components/SnsPost";
 
 const ProfileMyPost = () => {
   return (
     <React.Fragment>
-      <Wrap>
-        <Icon></Icon>
-        <SmallWrap>
-          <Title>내가 쓴 글</Title>
-          <Div>
-            <Title style={{ marginLeft: "-1.5rem" }}>Trils</Title>
-            <div>더보기</div>
-          </Div>
-          <Wrap
-            style={{
-              width: "78rem",
-              marginLeft: "-4rem",
-            }}
-          >
-            <SnsPost />
-            <SnsPost />
-            <SnsPost />
-          </Wrap>
-          <div
-            style={{
-              width: "79.5rem",
-              height: "3rem",
-              marginLeft: "-1rem",
-              borderBottom: "3px solid #89ACFF",
-            }}
-          ></div>
-        </SmallWrap>
-      </Wrap>
+      <ColumnWrap>
+        <Wrap>
+          <Icon></Icon>
+          <ColumnWrap>
+            <Title>내가 쓴 글</Title>
+            <Div>
+              <Title style={{ marginLeft: "-1.5rem" }}>Trils</Title>
+              <Button>더보기</Button>
+            </Div>
+            <Wrap
+              style={{
+                width: "78rem",
+                marginLeft: "-4rem",
+              }}
+            >
+              <SnsPost />
+              <SnsPost />
+              <SnsPost />
+            </Wrap>
+            <div
+              style={{
+                width: "79rem",
+                height: "3rem",
+                marginLeft: "-1rem",
+                marginBottom: "3rem",
+                borderBottom: "3px solid #89ACFF",
+              }}
+            ></div>
+          </ColumnWrap>
+        </Wrap>
+
+        <Wrap style={{ marginLeft: "7rem" }}>
+          <ColumnWrap>
+            <Div>
+              <Title style={{ marginLeft: "-1.5rem" }}>Trilog</Title>
+              <Button>더보기</Button>
+            </Div>
+            <Wrap
+              style={{
+                width: "78rem",
+                marginBottom: "5rem",
+              }}
+            >
+              <BoardCard margin="0 40px 0 0" />
+              <BoardCard margin="0 40px 0 0" />
+              <BoardCard margin="0 40px 0 0" />
+              <BoardCard margin="0 40px 0 0" />
+              <BoardCard margin="0 40px 0 0" />
+            </Wrap>
+            <div
+              style={{
+                width: "79rem",
+                height: "3rem",
+                marginLeft: "-1rem",
+                borderBottom: "3px solid #89ACFF",
+              }}
+            ></div>
+          </ColumnWrap>
+        </Wrap>
+      </ColumnWrap>
     </React.Fragment>
   );
 };
@@ -49,10 +81,9 @@ const Wrap = styled.div`
   width: auto;
 `;
 
-const SmallWrap = styled.div`
+const ColumnWrap = styled.div`
   display: flex;
   flex-direction: column;
-  width: 22rem;
 `;
 
 const Div = styled.div`
@@ -76,4 +107,13 @@ const Title = styled.div`
   margin-left: 1rem;
   margin-bottom: 3rem;
   font-size: 1.2rem;
+`;
+
+const Button = styled.button`
+  width: 3.5rem;
+  height: 1.5rem;
+  border: 1px solid #989898;
+  border-radius: 5px;
+  background-color: #ffffff;
+  padding: 0.2rem;
 `;
