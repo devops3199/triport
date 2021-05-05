be817a093a401ac86f97f80124b434f012e647c0
kbyunghoon <juice1231102@naver.com>
2021-05-05 21:47:29 +0900

    영상뷰 수정

--- src/pages/ReelsTest.js
+++ src/pages/ReelsTest.js
@@ -1,22 +1,29 @@
 import Video from "components/Video";
 import React from "react";
 import styled from "styled-components";
-// import Video1 from "../media/test.mp4";
+import Video1 from "../media/test.mp4";
 
 const ReelsTest = () => {
   return (
     <Test>
       <CenterDiv>
-        <InstaLogo>
-          <ImgLogo />
-        </InstaLogo>
-        <h3>Reel</h3>
         <VideoContainer>
           <Video
             key="1"
             channel="ccc"
             song="song-3"
-            // url={Video1}
+            url={Video1}
+            likes="89"
+            comment="23"
+            shares="29"
+          />
+        </VideoContainer>{" "}
+        <VideoContainer>
+          <Video
+            key="1"
+            channel="ccc"
+            song="song-3"
+            url={Video1}
             likes="89"
             comment="23"
             shares="29"
@@ -33,25 +40,20 @@ const Test = styled.div`
 `;
 
 const CenterDiv = styled.div`
+  display: flex;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  text-align: center;
-  transform: translate(-50%, -50%);
+  flex-direction: row;
+  width: 80rem;
 `;
 
-const InstaLogo = styled.div`
-  width: 50%;
-`;
+const InstaLogo = styled.div``;
 
 const ImgLogo = styled.div``;
 
 const VideoContainer = styled.div`
-  height: 15vh;
-  width: 20vw;
   max-width: 500px;
-  border-radius: 20px;
   background-color: white;
+  margin: 0 auto;
 `;
 
 export default ReelsTest;
